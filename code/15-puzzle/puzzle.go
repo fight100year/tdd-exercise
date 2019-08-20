ffa8092c418762a794ef1f8ec7df4ea393ccbbce
63isOK <1876180681@qq.com>
2019-08-20 06:12:30 +0000

    打印拼图 绿灯

--- code/15-puzzle/puzzle.go
+++ code/15-puzzle/puzzle.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+)
+
+func init() {
+
+}
+
 var (
 	table = [16]int{}
 )
@@ -10,8 +18,21 @@ func createTable() string {
 `
 }
 
-func printTable() string {
-	return ``
+func printTable() (result string) {
+	result += fmt.Sprintln()
+	for i, element := range table {
+		if element == 0 {
+			result += "   "
+		} else {
+			result += fmt.Sprintf(" %02d", element)
+		}
+
+		if (i+1)%4 == 0 {
+			result += fmt.Sprintln()
+		}
+	}
+
+	return
 }
 
 func main() {
