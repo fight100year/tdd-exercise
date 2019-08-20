63d7de8ff6638a5f33cf813732a4441199e69839
63isOK <1876180681@qq.com>
2019-08-20 01:10:42 +0000

    绿灯

--- code/100-doors/doors.go
+++ code/100-doors/doors.go
@@ -9,8 +9,25 @@ func Door() []int {
 	return configDoor(100)
 }
 
+func changeDoor(state int) int {
+	if state == 1 {
+		return 0
+	}
+
+	return 1
+}
+
 func configDoor(countDoor int) []int {
-	return []int{}
+	doors := make([]int, countDoor+1)
+	for i := 1; i <= countDoor; i++ {
+		j := i
+		for j <= countDoor {
+			doors[j] = changeDoor(doors[j])
+			j += i
+		}
+	}
+
+	return doors[1:]
 }
 
 func main() {
