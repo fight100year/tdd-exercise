104d39e970d23aed444e7372d18c765d3ad66840
63isOK <1876180681@qq.com>
2019-08-23 02:46:32 +0000

    添加最小实现

--- code/palindrome/palindrome.go
+++ code/palindrome/palindrome.go
@@ -1,5 +1,17 @@
 package palindrome
 
+import (
+	"strings"
+)
+
+// CheckPalindrome check a is palindrome or not
 func CheckPalindrome(testString string) bool {
-	return true
+	testString = strings.ToLower(testString)
+
+	reverseString := ""
+	for _, x := range testString {
+		reverseString = string(x) + reverseString
+	}
+
+	return reverseString == testString
 }
